ca5c7761f37fcccd8561ed9fb59d89adfb448fb3
Kleber <kleberalmendro01@gmail.com>
2021-08-26 15:03:18 -0300

    First Commit With the project

---
+++ dio.santander/README.md
@@ -0,0 +1,3 @@
+# Construindo um projeto baseado em microsservições usando Spring Cloud
+
+- Github do professor: https://github.com/oswaldoneto/dio-experts
---
+++ dio.santander/shopping-cart/src/main/java/dio/experts/shoppingcart/repository/CartRepository.java
@@ -0,0 +1,10 @@
+package dio.experts.shoppingcart.repository;
+
+import dio.experts.shoppingcart.model.Cart;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CartRepository extends CrudRepository<Cart, Integer> {
+
+    Cart findByCustomerId(Integer customerId);
+
+}
